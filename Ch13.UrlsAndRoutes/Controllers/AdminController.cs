236c794a72b3bbf64c5cb9642d195bcbf18a0887
Valeriu Caraulean <valeriu.caraulean@b-i.com>
2013-11-05 11:34:18 +0100

    Chapter 13, initial projects and few controllers to get started

---
+++ Ch13.UrlsAndRoutes/Controllers/AdminController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ch13.UrlsAndRoutes.Controllers
+{
+    public class AdminController : Controller
+    {
+        //
+        // GET: /Admin/
+
+        public ActionResult Index()
+        {
+            ViewBag.Controller = "Admin";
+            ViewBag.Action = "Index";
+            return View("ActionName");
+        }
+    }
+}
