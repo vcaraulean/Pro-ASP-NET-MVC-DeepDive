b37d012853b7ccbcc9236684de227dcf58e6195f
Valeriu Caraulean <valeriu.caraulean@b-i.com>
2013-11-05 16:20:19 +0100

    Chapter 14, cleaned up the UrlsAndRoutes project for Chapter 14

--- Ch13.UrlsAndRoutes/App_Start/RouteConfig.cs
+++ Ch13.UrlsAndRoutes/App_Start/RouteConfig.cs
@@ -11,18 +11,7 @@ namespace Ch13.UrlsAndRoutes
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
-            //var route = new Route("{controller}/{action}", new MvcRouteHandler());
-            //routes.Add("myRoute", route);
-
-            routes.MapRoute("ShopSchema2", "Shop/OldAction", new { controller = "Home", action = "index" });
-
-            routes.MapRoute("ShopSchema", "Shop/{action}", new { controller = "Home"});
-
-            routes.MapRoute("", "X{controller}/{action}");
-
-            routes.MapRoute("myRoute", "{controller}/{action}/{id}/{*catchall}", new { controller = "Home", action = "Index", id = UrlParameter.Optional });
-
-            routes.MapRoute("", "Public/{controller}/{action}", new { controller = "Home", action = "Index" });
+            routes.MapRoute("myRoute", "{controller}/{action}/{id}", new { controller = "Home", action = "Index", id = UrlParameter.Optional });
         }
     }
 }
