ec557f75de76dc49809072ae896f056aa08c1ba3
Valeriu Caraulean <valeriu.caraulean@b-i.com>
2013-11-06 12:13:13 +0100

    Chapter 16, created Filters project

---
+++ Filters/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Filters.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return "This is the Index action on the Home controller";
+        }
+	}
+}
