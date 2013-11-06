90a2870adb8cd89c90ec91ef3f09d09f728c16cf
Valeriu Caraulean <valeriu.caraulean@b-i.com>
2013-11-06 15:05:56 +0100

    Chapter 16, completed

--- Filters/Controllers/HomeController.cs
+++ Filters/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Filters.Infrastructure;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,11 +7,35 @@ using System.Web.Mvc;
 
 namespace Filters.Controllers
 {
+    [CustomAuth(true)]
     public class HomeController : Controller
     {
         public string Index()
         {
             return "This is the Index action on the Home controller";
+        }
+
+        [RangeException]
+        public string Range(int id)
+        {
+            if (id > 100)
+            {
+                return string.Format("The ID value is {0}", id);
+            }
+
+            throw new ArgumentOutOfRangeException("id", id, "");
+        }
+
+        [CustomAction]
+        public string FilterTest()
+        {
+            return "Filter action test";
+        }
+
+        [ProfileAction]
+        public string FilterTest2()
+        {
+            return "Profiled action";
         }
 	}
 }
---
+++ Filters/Infrastructure/ProfileActionAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Filters.Infrastructure
+{
+    public class ProfileActionAttribute : FilterAttribute, IActionFilter
+    {
+        Stopwatch stopwatch;
+        public void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            stopwatch.Stop();
+
+            filterContext.HttpContext.Response.Write(string.Format("<div>Action elapsed time: {0}</div>", stopwatch.Elapsed.TotalSeconds));
+        }
+
+        public void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            stopwatch = Stopwatch.StartNew();
+        }
+    }
+}
