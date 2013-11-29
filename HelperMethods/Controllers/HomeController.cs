7c272618ab00ad4d2ecca5ec3e73bfa0795355f7
Valeriu Caraulean <valeriu.caraulean@b-i.com>
2013-11-29 15:54:10 +0100

    Chapter 20, completed

--- HelperMethods/Controllers/HomeController.cs
+++ HelperMethods/Controllers/HomeController.cs
@@ -30,7 +30,7 @@ namespace HelperMethods.Controllers
         [HttpPost]
         public ActionResult CreatePerson(Person person)
         {
-            return View(person);
+            return View("DisplayPerson", person);
         }
 
     }
--- HelperMethods/Models/Person.cs
+++ HelperMethods/Models/Person.cs
@@ -1,15 +1,27 @@
 ﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
 
 namespace HelperMethods.Models
 {
+    [DisplayName("New person")]
     public class Person
     {
+        [HiddenInput(DisplayValue = false)]
         public int PersonId { get; set; }
+        [DisplayName("First")]
         public string FirstName { get; set; }
+        [DisplayName("Last")]
         public string LastName { get; set; }
+        
+        [DataType(DataType.Date)]
         public DateTime Birthday { get; set; }
         public Address Address { get; set; }
+        
+        [DisplayName("Approuved")]
         public bool IsApproved { get; set; }
+        [UIHint("Enum")]
         public Role Role { get; set; }
     }
 
