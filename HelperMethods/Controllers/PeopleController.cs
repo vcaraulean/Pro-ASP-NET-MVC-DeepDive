4beca665d1e564788cb6eef11dcd3537e6712179
Valeriu Caraulean <valeriu.caraulean@b-i.com>
2013-11-29 17:04:24 +0100

    Chapter 21, in progress

---
+++ HelperMethods/Controllers/PeopleController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HelperMethods.Models;
+
+namespace HelperMethods.Controllers
+{
+    public class PeopleController : Controller
+    {
+        private Person[] personData =
+        {
+            new Person {FirstName = "Adam", LastName = "Freeman", Role = Role.Admin},
+            new Person {FirstName = "Steven", LastName = "Sanderson", Role = Role.Admin},
+            new Person {FirstName = "Jacqui", LastName = "Griffyth", Role = Role.User},
+            new Person {FirstName = "John", LastName = "Smith", Role = Role.User},
+            new Person {FirstName = "Anne", LastName = "Jones", Role = Role.Guest},
+        };
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetPeople()
+        {
+            return View(personData);
+        }
+
+        [HttpPost]
+        public ActionResult GetPeople(string selectedRole)
+        {
+            if (selectedRole == null || selectedRole == "ALL")
+                return View(personData);
+
+            var selected = (Role) Enum.Parse(typeof(Role), selectedRole);
+            return View(personData.Where(p => p.Role == selected));
+        }
+    }
+}
