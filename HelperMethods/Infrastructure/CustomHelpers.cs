86b5a797446c1c41e59e1c723205f248721962d4
Valeriu Caraulean <valeriu.caraulean@b-i.com>
2013-11-29 12:30:18 +0100

    Chapter 19, completed

---
+++ HelperMethods/Infrastructure/CustomHelpers.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace HelperMethods.Infrastructure
+{
+    public static class CustomHelpers
+    {
+        public static MvcHtmlString ListArrayItems(this HtmlHelper html, IEnumerable<string> list)
+        {
+            var tag = new TagBuilder("ul");
+            foreach (var item in list)
+            {
+                var itemTag = new TagBuilder("li");
+                itemTag.SetInnerText(item);
+                tag.InnerHtml += itemTag.ToString();
+            }
+            return new MvcHtmlString(tag.ToString());
+        }
+
+        public static MvcHtmlString DisplayMessage(this HtmlHelper html, string msg)
+        {
+            var encoded = html.Encode(msg);
+            var result = string.Format("This is the message: <p>{0}</p>", encoded);
+            return new MvcHtmlString(result);
+        }
+    }
+}
---
+++ HelperMethods/Models/Person.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace HelperMethods.Models
+{
+    public class Person
+    {
+        public int PersonId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime Birthday { get; set; }
+        public Address Address { get; set; }
+        public bool IsApproved { get; set; }
+        public Role Role { get; set; }
+    }
+
+    public enum Role
+    {
+        Admin, User, Guest
+    }
+
+    public class Address
+    {
+        public string Line1 { get; set; }
+        public string Line2 { get; set; }
+        public string City { get; set; }
+        public string PostCode { get; set; }
+        public string Country { get; set; }
+    }
+}
