6f3db3bb5d2dc831f21f6db9e33212a2ade7803f
Valeriu Caraulean <valeriu.caraulean@b-i.com>
2013-12-03 18:02:16 +0100

    Chapter 25, continued...

---
+++ WebServices/Controllers/ReservationController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+using WebServices.Models;
+
+namespace WebServices.Controllers
+{
+    public class ReservationController : ApiController
+    {
+        readonly IReservationRepository repository = ReservationRepository.GetRepository();
+
+        public IEnumerable<Reservation> GetAllReservations()
+        {
+            return repository.GetAll();
+        }
+
+        public Reservation GetReservation(int id)
+        {
+            return repository.Get(id);
+        }
+
+        public Reservation PostReservation(Reservation item)
+        {
+            return repository.Add(item);
+        }
+
+        [HttpPut]
+        public bool UpdateReservation(Reservation item)
+        {
+            return repository.Update(item);
+        }
+
+        public void DeleteReservation(int id)
+        {
+            repository.Remove(id);
+        }
+    }
+}
