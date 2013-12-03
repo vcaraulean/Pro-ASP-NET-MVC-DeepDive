fcfa0cb9bfae8ec8b72b06d480fb9db5fe638cee
Valeriu Caraulean <valeriu.caraulean@b-i.com>
2013-12-03 16:49:27 +0100

    Chapter 24, completed

---
+++ ClientFeatures/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ClientFeatures.Models;
+
+namespace ClientFeatures.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult MakeBooking()
+        {
+            return View(new Appointment
+            {
+                ClientName = "Microsoft",
+                Date = DateTime.Now.AddDays(3),
+                TermsAccepted = true,
+            });
+        }
+
+        [HttpPost]
+        public JsonResult MakeBooking(Appointment appointment)
+        {
+            return Json(appointment, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ ClientFeatures/Scripts/MakeBooking.js
@@ -0,0 +1,23 @@
+﻿function processResponse(appt) {
+    $("#successClientName").text(appt.ClientName);
+    $("#successDate").text(processDate(appt.Date));
+    switchViews();
+}
+
+var processDate = function (date) {
+    return new Date(parseInt(date.substr(6, date.length - 8))).toDateString();
+};
+
+var switchViews = function () {
+    var hidden = $(".hidden");
+    var visible = $(".visible");
+
+    hidden.removeClass("hidden").addClass("visible");
+    visible.removeClass("visible").addClass("hidden");
+};
+
+$(document).ready(function () {
+    $("#backButton").click(function (e) {
+        switchViews();
+    });
+});
